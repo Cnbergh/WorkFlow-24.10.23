ff494061fde0998acafb7b7a7a90286c3072487b
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-10-24 09:38:07 +0200

    function test

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 import "dotenv/config";
 
-console.log("hello world");
+function test(expectedSecret) {
+  const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+  console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+  console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+  return secretMatches;
+}
+
+test(42);
